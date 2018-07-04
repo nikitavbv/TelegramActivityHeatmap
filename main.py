195b3f6102fa1d6252bc996bafeaf2e9b62bd942
Nikita Volobuev <nikitavbv@gmail.com>
2018-07-04 13:19:00 +0300

    feat(main): database connection

--- main.py
+++ main.py
@@ -4,19 +4,41 @@ import time
 import datetime
 import schedule
 import os.path
+import sqlite3
 
 CONFIG_FILE_NAME = 'config.yaml'
+DATABASE_NAME = 'activity.db'
 STATUS_OFFLINE = 'offline'
 STATUS_ONLINE = 'online'
 
 config = yaml.load(open(CONFIG_FILE_NAME))
 interval = config['interval']
 
+db = None
+
 client = TelegramClient('activity_script', config['api_id'], config['api_hash']).start()
 
+def init_database():
+    global db
+    db = sqlite3.connect(DATABASE_NAME)
+
+    if not db_table_exists('activity'):
+        print('Creating activity database table')
+        db.execute('CREATE TABLE activity (group_id integer, user_id integer, online boolean)')
+
+    if not db_table_exists('users'):
+        print('Creating users database table')
+        db.execute('CREATE TABLE users (id interger, username text, first_name text, last_name text)')
+
+
+def db_table_exists(table_name):
+    query = db.execute('SELECT name FROM sqlite_master WHERE type="table" AND name=?', (table_name, ))
+    return query.fetchone() is not None
+
+
 def record_user_activity(group_id, user_id, username, first_name, last_name, status):
-    # save it to file
-    pass
+    print(group_id, user_id, username, first_name, last_name, status)
+
 
 def get_user_status(user):
     time_now = datetime.datetime.utcnow()
@@ -36,13 +58,13 @@ def process_statuses_for_chat(group_id):
 
 def stats_job():
     print(time.strftime('%H:%M', time.gmtime()), '-', 'Running iteration')
-    dialogs = client.get_dialogs()
-    for dialog in dialogs:
+    for dialog in client.get_dialogs():
         if dialog.title in config['target_groups']:
             process_statuses_for_chat(dialog.entity.id)
 
 
 if __name__ == "__main__":
+    init_database()
     stats_job()
     schedule.every(interval).seconds.do(stats_job)
     while True:
